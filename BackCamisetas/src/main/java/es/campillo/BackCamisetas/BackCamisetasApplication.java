467e3658017fbc39260313d3c9d448e7a0ae2663
jjcmc01 <jcampillom@sescam.jccm.es>
2024-05-09 16:43:10 +0200

    CE-T2 Creación proyecto Base BackEnd

---
+++ BackCamisetas/src/main/java/es/campillo/BackCamisetas/BackCamisetasApplication.java
@@ -0,0 +1,13 @@
+package es.campillo.BackCamisetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCamisetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCamisetasApplication.class, args);
+	}
+
+}
---
+++ BackCamisetas/src/test/java/es/campillo/BackCamisetas/BackCamisetasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.campillo.BackCamisetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCamisetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
